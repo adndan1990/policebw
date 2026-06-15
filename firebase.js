d02dd705735bc2dfac7355763fc32838464355e9
adndan1990 <lejardindugeek@gmail.com>
2026-06-16 01:52:09 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -4,38 +4,31 @@ import {
   doc,
   setDoc,
   getDoc,
-  updateDoc,
   deleteDoc,
   collection,
   onSnapshot
 } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-firestore.js";
 
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-analytics.js";
-
 const firebaseConfig = {
   apiKey: "AIzaSyCTZFF1QBbNS2AhY2My3FoxE3iMF2VrRso",
   authDomain: "police-bw.firebaseapp.com",
   projectId: "police-bw",
-  storageBucket: "police-bw.firebasestorage.app",
+  storageBucket: "police-bw.appspot.com",
   messagingSenderId: "606303723293",
-  appId: "1:606303723293:web:2df34f7a461d42d1580bf4",
-  measurementId: "G-20HRYJBSP0"
+  appId: "1:606303723293:web:2df34f7a461d42d1580bf4"
 };
 
-// INIT
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const db = getFirestore(app);
 
-// EXPORT GLOBAL POUR TES AUTRES FICHIERS
-window.dbInstance = db;
+// 🔥 IMPORTANT : export explicite
+export const db = getFirestore(app);
 
-window.firestoreTools = {
+export {
   doc,
   setDoc,
   getDoc,
-  updateDoc,
   deleteDoc,
   collection,
   onSnapshot
 };
+
