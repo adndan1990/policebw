269af1186773e24a3ea26d58fa66f7b2ac63a623
adndan1990 <lejardindugeek@gmail.com>
2026-06-16 01:37:30 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,22 +1,41 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-app.js";
 import {
-    getFirestore,
-    doc,
-    setDoc,
-    getDoc,
-    updateDoc,
-    deleteDoc,
-    collection,
-    onSnapshot
+  getFirestore,
+  doc,
+  setDoc,
+  getDoc,
+  updateDoc,
+  deleteDoc,
+  collection,
+  onSnapshot
 } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-firestore.js";
 
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-analytics.js";
+
 const firebaseConfig = {
-    apiKey: "TON_API_KEY",
-    authDomain: "police-bw.firebaseapp.com",
-    projectId: "police-bw",
-    storageBucket: "police-bw.appspot.com",
-    messagingSenderId: "606303723293",
-    appId: "1:606303723293:web:2df34f7a461d42d1580bf4"
+  apiKey: "AIzaSyCTZFF1QBbNS2AhY2My3FoxE3iMF2VrRso",
+  authDomain: "police-bw.firebaseapp.com",
+  projectId: "police-bw",
+  storageBucket: "police-bw.firebasestorage.app",
+  messagingSenderId: "606303723293",
+  appId: "1:606303723293:web:2df34f7a461d42d1580bf4",
+  measurementId: "G-20HRYJBSP0"
 };
 
-const app =
+// INIT
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const db = getFirestore(app);
+
+// EXPORT GLOBAL POUR TES AUTRES FICHIERS
+window.dbInstance = db;
+
+window.firestoreTools = {
+  doc,
+  setDoc,
+  getDoc,
+  updateDoc,
+  deleteDoc,
+  collection,
+  onSnapshot
+};
