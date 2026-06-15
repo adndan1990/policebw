dc661f815602ad4ea199baa7f7eefb02d32ee4e1
adndan1990 <lejardindugeek@gmail.com>
2026-06-16 01:15:10 +0200

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,34 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-app.js";
+import {
+getFirestore,
+doc,
+setDoc,
+getDoc,
+updateDoc,
+deleteDoc,
+collection,
+onSnapshot
+} from "https://www.gstatic.com/firebasejs/11.10.0/firebase-firestore.js";
+
+const firebaseConfig = {
+apiKey: "TON_KEY",
+authDomain: "police-bw.firebaseapp.com",
+projectId: "police-bw",
+storageBucket: "police-bw.appspot.com",
+messagingSenderId: "606303723293",
+appId: "1:606303723293:web:2df34f7a461d42d1580bf4"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export {
+db,
+doc,
+setDoc,
+getDoc,
+updateDoc,
+deleteDoc,
+collection,
+onSnapshot
+};
