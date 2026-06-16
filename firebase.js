98ad865c2a4a15e1ce615908532ad60b8af9a44b
adndan1990 <lejardindugeek@gmail.com>
2026-06-16 02:32:07 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -4,11 +4,15 @@ import {
   doc,
   setDoc,
   getDoc,
+  updateDoc,
   deleteDoc,
   collection,
   onSnapshot
 } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-firestore.js";
 
+/* =========================
+   CONFIG FIREBASE
+========================= */
 const firebaseConfig = {
   apiKey: "AIzaSyCTZFF1QBbNS2AhY2My3FoxE3iMF2VrRso",
   authDomain: "police-bw.firebaseapp.com",
@@ -18,15 +22,21 @@ const firebaseConfig = {
   appId: "1:606303723293:web:2df34f7a461d42d1580bf4"
 };
 
+/* =========================
+   INIT APP
+========================= */
 const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
-// 🔥 IMPORTANT : export explicite
-export const db = getFirestore(app);
-
+/* =========================
+   EXPORTS PROPRES
+========================= */
 export {
+  db,
   doc,
   setDoc,
   getDoc,
+  updateDoc,
   deleteDoc,
   collection,
   onSnapshot
